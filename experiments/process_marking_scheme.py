3c883f75b7eca87ed52fa71904157984fe69c296
xavilien <xavilien@gmail.com>
2021-10-07 01:12:57 +0800

    Save marking scheme as npy array for future use

--- experiments/process_marking_scheme.py
+++ experiments/process_marking_scheme.py
@@ -1,20 +1,26 @@
 from src import processing
 import pickle
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
 
 
 def get_marking_scheme():
-    with open("q1_marking_scheme.txt", "r") as file1, \
-            open("../src/Arrays/word_idx.pickle", "rb") as file2:
-        marking_scheme = [line for line in file1]
-        marking_scheme = " ".join(marking_scheme)
-        marking_scheme = processing.clean_tokenize(marking_scheme)
+    try:
+        marking_scheme = np.load("marking_scheme.npy")
+    except FileNotFoundError:
+        with open("q1_marking_scheme.txt", "r") as file1, \
+                open("../src/Arrays/word_idx.pickle", "rb") as file2:
+            marking_scheme = [line for line in file1]
+            marking_scheme = " ".join(marking_scheme)
+            marking_scheme = processing.clean_tokenize(marking_scheme)
 
-        word_idx = pickle.load(file2)
+            word_idx = pickle.load(file2)
 
-        marking_scheme = [word_idx.get(word, word) for word in marking_scheme]
+            marking_scheme = [word_idx.get(word, word) for word in marking_scheme]
 
-        marking_scheme = pad_sequences([marking_scheme], maxlen=152)
+            marking_scheme = pad_sequences([marking_scheme], maxlen=152)
+
+            np.save("marking_scheme.npy", marking_scheme)
 
     return marking_scheme
 
